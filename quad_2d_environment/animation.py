f139dd2f1fceb1517bdaf4d750adb9c091272382
NicoOhR <nohayonr@gmail.com>
2024-12-23 02:33:13 -0600

    human input

--- quad_2d_environment/animation.py
+++ quad_2d_environment/animation.py
@@ -1,7 +1,7 @@
 from quadcopter import Quadcopter2d
 import matplotlib.pyplot as plt
-import math
 import matplotlib.animation as animation
+import math
 
 
 class QuadAnimation:
@@ -11,47 +11,54 @@ class QuadAnimation:
 
         self.fig, self.ax = plt.subplots()
 
-        self.quadcopter_body, = self.ax.plot([], [], 'o-', lw=2, label="Quadcopter")
-        self.trail, = self.ax.plot([], [], 'r--', lw=1, label="Trail")
+        (self.quadcopter_body,) = self.ax.plot([], [], "o-", lw=2, label="Quadcopter")
+        (self.trail,) = self.ax.plot([], [], "r--", lw=1, label="Trail")
         self.time_template = "Time: {:.1f}s"
-        self.time_text = self.ax.text(0.02, 0.95, '', transform=self.ax.transAxes)
-        self.ax.set_xlim(-50, 50) 
-        self.ax.set_ylim(0, 100) 
+        self.time_text = self.ax.text(0.02, 0.95, "", transform=self.ax.transAxes)
+        self.ax.set_xlim(-50, 50)
+        self.ax.set_ylim(0, 100)
 
         self.trajectory_x = []
         self.trajectory_y = []
         self.quadcopter_body.set_data([], [])
         self.trail.set_data([], [])
-        self.time_text.set_text('')
+        self.time_text.set_text("")
         self.quad = Quadcopter2d()
 
     def init_animation(self):
         self.quadcopter_body.set_data([], [])
         self.trail.set_data([], [])
-        self.time_text.set_text('')
+        self.time_text.set_text("")
         return self.quadcopter_body, self.trail, self.time_text
 
     def update(self, frame):
 
-        (e1, e2) = self.quad.edges() 
+        (e1, e2) = self.quad.edges()
         self.quadcopter_body.set_data(e1, e2)
 
         self.trajectory_x.append(self.quad.state[0])
         self.trajectory_y.append(self.quad.state[1])
         self.trail.set_data(self.trajectory_x, self.trajectory_y)
         self.time_text.set_text(self.time_template.format(frame * self.dt))
-
-        u1,u2 = self.quad.state[3:5]
+        u1, u2 = self.quad.state[3:5]
         self.quad.update(u1, u2, self.dt)
         self.quad.crash()
         return self.quadcopter_body, self.trail, self.time_text
 
     def run(self):
-        ani = animation.FuncAnimation(self.fig,self.update, frames=500, init_func=self.init_animation, blit=True, interval=self.dt*1000)
+        self.quad.human_input()
+        ani = animation.FuncAnimation(
+            self.fig,
+            self.update,
+            frames=500,
+            init_func=self.init_animation,
+            blit=True,
+            interval=self.dt * 1000,
+        )
         plt.show()
         plt.close(self.fig)
+
 
 if __name__ == "__main__":
     quad_anim = QuadAnimation()
     quad_anim.run()
-
--- quad_2d_environment/quadcopter.py
+++ quad_2d_environment/quadcopter.py
@@ -1,20 +1,24 @@
-import math
 import scipy.constants
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import math
+from pynput import keyboard
+from pynput.keyboard import Controller
+
 
 class Quadcopter2d:
-    def __init__(self, mass = 5, r = 5):
-        self.mass = mass 
+    def __init__(self, mass=5, r=5):
+        self.mass = mass
         self.r = r
-        self.mI = ((r*2) ** 2) * mass * 1/12 
-        self.state = np.array([10, 10, 0, 12, 10, 0]) #[x, y, theta, u1, u2, omega]
+        self.mI = ((r * 2) ** 2) * mass * 1 / 12
+        self.state = np.array([10, 10, 0, 0, 0, 0])  # [x, y, theta, u1, u2, omega]
+        self.keyboard = Controller()
 
-    def dynamics(self, u1,u2,theta):
-        acc_x = -(u1 + u2) * math.sin(theta) * 1/self.mass
-        acc_y = (u1 + u2) * math.cos(theta) * 1/self.mass - scipy.constants.g 
-        acc_ang = self.r * (u1 - u2) * 1/self.mI
+    def dynamics(self, u1, u2, theta):
+        acc_x = -(u1 + u2) * math.sin(theta) * 1 / self.mass
+        acc_y = (u1 + u2) * math.cos(theta) * 1 / self.mass - scipy.constants.g
+        acc_ang = self.r * (u1 - u2) * 1 / self.mI
         return (acc_x, acc_y, acc_ang)
 
     def update(self, u1, u2, dt):
@@ -25,11 +29,12 @@ class Quadcopter2d:
         vy += acc[1] * dt
         omega += acc[2] * dt
 
-        x += vx * dt 
+        x += vx * dt
         y += vy * dt
         theta += omega * dt
-        
+
         self.state = np.array([x, y, theta, vx, vy, omega])
+
     def edges(self):
         x, y, theta = self.state[0:3]
         x1 = x - self.r * math.cos(theta)
@@ -37,12 +42,28 @@ class Quadcopter2d:
         x2 = x + self.r * math.cos(theta)
         y2 = y + self.r * math.sin(theta)
 
-        #Floating point precision causes +/- 1e-7 of error
-        #I simply cannot escape IEEE 754 
+        # Floating point precision causes +/- 1e-7 of error
+        # I simply cannot escape IEEE 754
         d = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
-        return ([x1,x2], [y1,y2])
+        return ([x1, x2], [y1, y2])
 
     def crash(self):
-        #ich bein un haskller
-        return(any(map(lambda x: x<0, sum(self.edges(), []))))
+        # ich bein un haskller
+        return any(map(lambda x: x < 0, sum(self.edges(), [])))
+
+    def input(self, u1, u2):
+        self.state[3] = max(self.state[3] + 3, u1) if self.state[3] - u1 >= 3 else u1
+        self.state[4] = max(self.state[4] + 3, u2) if self.state[4] - u2 >= 3 else u2
+
+    def on_press(self, key):
+        if key.char == "u":
+            print("left")
+            self.input(self.state[3] + 3, self.state[4])
+        if key.char == "i":
+            print("right")
+            self.input(self.state[3], self.state[4] + 3)
+
+    def human_input(self):
+        listener = keyboard.Listener(on_press=self.on_press)
+        listener.start()
