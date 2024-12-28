4ea17b41b13686a26f0bdbb5100281792b634512
NicoOhR <nohayonr@gmail.com>
2024-12-27 23:25:06 -0600

    testing trained model, is bad

---
+++ quad-planar/environment/.ipynb_checkpoints/render-checkpoint.py
@@ -0,0 +1,57 @@
+from .quadcopter import Quadcopter2d
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import math
+
+import matplotlib.pyplot as plt
+import time
+
+
+class QuadRender:
+    def __init__(self, quad):
+        self.dt = 0.0001
+        self.quad = quad
+        self.initialize_plot()
+
+    def initialize_plot(self):
+        plt.ion()
+        self.fig, self.ax = plt.subplots()
+        (self.quadcopter_body,) = self.ax.plot([], [], "o-", lw=2, label="Quadcopter")
+        (self.trail,) = self.ax.plot([], [], "r--", lw=1, label="Trail")
+        self.time_text = self.ax.text(0.02, 0.95, "", transform=self.ax.transAxes)
+        self.ax.set_xlim(-50, 50)
+        self.ax.set_ylim(0, 100)
+        self.trajectory_x = []
+        self.trajectory_y = []
+        self.quadcopter_body.set_data([], [])
+        self.trail.set_data([], [])
+        self.time_text.set_text("")
+
+    def render(self, frame):
+        edges = self.quad.edges()
+        state = self.quad.physics_state
+        e1, e2 = edges
+        self.quadcopter_body.set_data(e1, e2)
+        self.trajectory_x.append(state[0])
+        self.trajectory_y.append(state[1])
+        # self.trail.set_data(self.trajectory_x, self.trajectory_y)
+        self.time_text.set_text(f"Time: {frame * self.dt:.1f}s")
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
+
+
+if __name__ == "__main__":
+    quad = Quadcopter2d(hid=True)
+    quad_anim = QuadRender(quad)
+    frame = 0
+    try:
+        while True:
+            quad.update()
+            quad.human_input()
+            quad_anim.render(frame)
+            frame += 1
+            time.sleep(quad_anim.dt)
+    except KeyboardInterrupt:
+        print("Animation stopped.")
+        plt.ioff()
+        plt.show()
---
+++ quad-planar/test.py
@@ -0,0 +1,21 @@
+from environment.render import QuadRender
+from agent.networks import REINFORCE
+import gymnasium as gym
+import torch
+import sys
+
+env = gym.make("QuadEnv-v0", render_mode="human")
+if env.observation_space.shape:
+    obs_space_dims = env.observation_space.shape[0]
+else:
+    sys.exit(-1)
+
+action_space_dims = 4
+model = REINFORCE(obs_space_dims, action_space_dims)
+model.net.load_state_dict(torch.load("trained/agent_episode_4000.pt"))
+
+done = False
+obs, info = env.reset()
+while not done:
+    action = model.sample_action(obs)
+    obs, reward, terminated, truncated, info = env.step(int(action))
